56f2a5c8db21dbf4fa18fcb24076b7b19596c7e3
mariuszadams <mariuszadams@gmail.com>
2020-02-19 00:58:27 +0100

    dbs2i

--- db/build-db.sh
+++ db/build-db.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo 'Initializing the database post-build...'
-mysql -uroot quotesdb < quote.sql
+#mysql -uroot quotesdb < quote.sql
+COPY quote.sql /docker-entrypoint-initdb.d/quote.sql
 echo 'DB initialized.'
